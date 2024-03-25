824dec74451dd585da132dfbbc8e5bc5507eafe7
macDanielZ <tsudaikamaru@gmail.com>
2024-03-25 16:52:25 +0700

    fix/mail_host

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,9 +1,7 @@
 var cacheName = 'hello-pwa';
 var filesToCache = [
   '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
+  '/login',
 ];
 
 /* Start the service worker and cache all of the app's content */
